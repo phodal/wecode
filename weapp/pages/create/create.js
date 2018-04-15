fdfef4cb6c3e8e3c2f7b609474c4ff7a4ba036c4
Phodal HUANG <h@phodal.com>
2018-04-15 14:48:54 +0800

    [weapp] init create page

---
+++ weapp/pages/create/create.js
@@ -0,0 +1,18 @@
+// pages/create.js
+Page({
+  data: {
+    height: 20,
+    focus: false
+  },
+  bindButtonTap: function () {
+    this.setData({
+      focus: true
+    })
+  },
+  bindTextAreaBlur: function (e) {
+    console.log(e.detail.value)
+  },
+  bindFormSubmit: function (e) {
+    console.log(e.detail.value.textarea)
+  }
+})
