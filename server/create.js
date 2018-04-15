4a0827c20415894090c43bdaf047ad4ad16ab83b
Phodal HUANG <h@phodal.com>
2018-04-15 23:04:32 +0800

    [T] add keywords checking

--- server/create.js
+++ server/create.js
@@ -4,6 +4,7 @@ const AWS = require('aws-sdk');
 const hljs = require('highlight.js');
 const marked = require('marked');
 const shortid = require('shortid');
+const tc = require('text-censor')
 
 AWS.config.setPromisesDependency(Promise);
 
@@ -13,16 +14,10 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 module.exports.create = (event, context, callback) => {
   let body;
   try {
-      body = JSON.parse(event.body);
+    body = JSON.parse(event.body);
   } catch (error) {
     console.log(error)
-    callback(
-      null,
-      {
-        statusCode: 400,
-        body: JSON.stringify(error)
-      }
-    );
+    callback(null, {statusCode: 400, body: JSON.stringify(error)});
   }
   const userId = body.user_id;
   const title = body.title;
@@ -31,49 +26,54 @@ module.exports.create = (event, context, callback) => {
   console.log(body)
 
   if (!userId || !code) {
-    callback(
-      null,
-      {
-        statusCode: 400,
-        body: '弄什么呢'
-      }
-    );
+    callback(null, {statusCode: 400, body: '弄什么呢'});
   }
 
   const timestamp = new Date().getTime();
-  const params = {
-    TableName: tableName,
-    Item: {
-      title: title,
-      id: shortid.generate(),
-      userId: userId,
-      code: codeToHtml(code),
-      createdAt: timestamp
+
+  tc.filter(code, function (err, censoredCode) {
+    if (err) {
+      callback(null, {statusCode: 500, body: JSON.stringify(err)});
     }
-  };
+    tc.filter(title, function (err, censoredTitle) {
+      if (err) {
+        callback(null, {statusCode: 500, body: JSON.stringify(err)});
+      }
 
-  console.log(params)
+      const params = {
+        TableName: tableName,
+        Item: {
+          title: censoredTitle,
+          id: shortid.generate(),
+          userId: userId,
+          code: codeToHtml(censoredCode),
+          createdAt: timestamp
+        }
+      };
 
-  docClient.put(params, (error) => {
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: {'Content-Type': 'text/plain'},
-        body: 'couldn\'t create the form item.',
+      docClient.put(params, (error) => {
+        if (error) {
+          console.error(error);
+          callback(null, {
+            statusCode: error.statusCode || 501,
+            headers: {'Content-Type': 'text/plain'},
+            body: 'couldn\'t create the form item.',
+          });
+          return;
+        }
+
+        const response = {
+          statusCode: 201,
+          headers: {
+            "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+          },
+          body: JSON.stringify(params.Item),
+        };
+        callback(null, response);
       });
-      return;
-    }
+    })
 
-    const response = {
-      statusCode: 201,
-      headers: {
-        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
-      },
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
-  });
+  })
 };
 
 function codeToHtml(code) {
