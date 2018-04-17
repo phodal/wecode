0fa4c02a6c0e6d784697645a243e1e120bb51a6c
Phodal HUANG <h@phodal.com>
2018-04-17 08:05:33 +0800

    [server] add post code interface

--- server/code.js
+++ server/code.js
@@ -63,3 +63,34 @@ module.exports.list = (event, context, callback) => {
     callback(null, response);
   });
 };
+
+module.exports.update = (event, context, callback) => {
+  let codeId = event.pathParameters.codeId;
+
+  const params = {
+    TableName: tableName,
+    FilterExpression: 'id = :id',
+    ExpressionAttributeValues: {
+      ':id': codeId,
+    }
+  };
+
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'couldn\'t fetch the logs.',
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      headers: {'Content-Type': 'text/html'},
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+}
