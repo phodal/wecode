07cca32d40067665f83c5fa390fab77d15b71e90
Phodal HUANG <h@phodal.com>
2018-04-17 09:05:00 +0800

    [server] add post interface for code update

--- server/code.js
+++ server/code.js
@@ -65,31 +65,58 @@ module.exports.list = (event, context, callback) => {
 };
 
 module.exports.update = (event, context, callback) => {
-  let codeId = event.pathParameters.codeId;
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (error) {
+    console.log(error)
+    callback(null, {statusCode: 400, body: JSON.stringify(error)});
+  }
+
+  const userId = body.user_id;
+  const code = body.code;
+  const title = body.title;
+  const codeId = event.pathParameters.codeId;
+
+  if (!userId || !code) {
+    callback(null, {statusCode: 400, body: '弄什么呢'});
+  }
 
   const params = {
     TableName: tableName,
-    FilterExpression: 'id = :id',
+    Key: {
+      id: codeId,
+      userId: userId
+    },
+    ExpressionAttributeNames: {
+      '#code': 'code',
+      '#title': 'title'
+    },
     ExpressionAttributeValues: {
-      ':id': codeId,
-    }
+      ':code': code,
+      '#title': title
+    },
+    UpdateExpression: 'SET #code = :code, title = :title',
+    ReturnValues: 'ALL_NEW',
   };
 
-  dynamoDb.scan(params, (error, result) => {
+  // update the todo in the database
+  dynamoDb.update(params, (error, result) => {
+    // handle potential errors
     if (error) {
       console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'couldn\'t fetch the logs.',
+        body: 'Couldn\'t fetch the todo item.',
       });
       return;
     }
 
+    // create a response
     const response = {
       statusCode: 200,
-      headers: {'Content-Type': 'text/html'},
-      body: JSON.stringify(result.Items),
+      body: JSON.stringify(result.Attributes),
     };
     callback(null, response);
   });
