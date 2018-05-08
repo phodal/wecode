f34d52d0478533ca1f48af8d432710ee6d63c329
Phodal HUANG <h@phodal.com>
2018-05-08 21:45:18 +0800

    [server] upload: it seems works

--- server/upload.js
+++ server/upload.js
@@ -9,15 +9,15 @@ module.exports.upload = (event, context, callback) => {
   let s3 = new AWS.S3();
   console.log(event.body)
   let buffer = new Buffer(event.body, 'base64');
-  let parsed = Utils.parse(event, true);
-  console.log(parsed);
+  let parsedBody = Utils.parse(event, true);
+  console.log(parsedBody);
+  let file = parsedBody.file;
 
   s3.putObject({
-    Body: buffer,
+    Body: file.content,
     Bucket: bucketName,
-    Key: shortid.generate() + '.jpg',
-    ContentEncoding: 'base64',
-    ContentType: 'image/jpeg'
+    Key: file.filename,
+    ContentType: file.contentType
   }, function (err, data) {
     if (err) {
       return callback(new Error(`Failed to put s3 object: ${err}`));
