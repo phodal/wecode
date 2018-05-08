e9b0e63b7d1c2d6a7bbfd3010d42d41b03c53ea5
Phodal HUANG <h@phodal.com>
2018-05-08 20:57:00 +0800

    [T] fix promise issue

--- server/upload.js
+++ server/upload.js
@@ -1,25 +1,30 @@
 var AWS = require('aws-sdk');
-var queryString = require('query-string');
 
 const shortid = require('shortid');
 const bucketName = process.env.S3;
 
 module.exports.upload = (event, context, callback) => {
   let s3 = new AWS.S3();
-  let buffer = Buffer.from(event.body, 'base64');
+  console.log(event.body)
+  let buffer = new Buffer(event.body, 'base64');
 
   s3.putObject({
     Body: buffer,
     Bucket: bucketName,
     Key: shortid.generate() + '.jpg',
-    ContentEncoding: 'base64'
-  });
+    ContentEncoding: 'base64',
+    ContentType: 'image/jpeg'
+  }, function (err, data) {
+    if (err) {
+      return callback(new Error(`Failed to put s3 object: ${err}`));
+    }
 
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({}),
+    return callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({})
+    })
   })
 }
