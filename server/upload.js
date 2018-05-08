40db723bb875b7d0312301ca5afec0473944cd47
Phodal HUANG <h@phodal.com>
2018-05-08 09:17:25 +0800

    [T] try to fix upload

--- server/upload.js
+++ server/upload.js
@@ -1,27 +1,24 @@
 var AWS = require('aws-sdk');
 var queryString = require('query-string');
 
-module.exports.upload = (event, context, callback) => {
+const shortid = require('shortid');
+const bucketName = process.env.S3;
 
-  // Create new S3 instance to handle our request for a new upload URL.
+module.exports.upload = (event, context, callback) => {
   var s3 = new AWS.S3();
 
-  // Parse out the parameters of the file the client would like to upload.
   var params = queryString.parse(event.body)
   console.log(params);
 
-  // Assemble a dictionary of parameters to hand to S3: the S3 bucket name, the file name, the file type, and permissions.  Other paramters like expiration can be specified here.  See the documentation for this method for more details.
   var s3Params = {
-    Bucket: '[bucketName]',
-    Key:  params.name,
-    ContentType: params.type,
+    Bucket: bucketName,
+    Key:  shortid.generate(),
+    ContentType: 'image/jpeg',
     ACL: 'public-read',
   };
 
-  // Ask S3 for a temporary URL that the client can use.
   var uploadURL = s3.getSignedUrl('putObject', s3Params);
 
-  // Return success message to the client, with the upload URL in the payload.
   callback(null, {
     statusCode: 200,
     headers: {
