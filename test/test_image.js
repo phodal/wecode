8bffd6eae7458846adc9930c6e0d0b92690a0275
Phodal HUANG <h@phodal.com>
2018-05-09 21:54:35 +0800

    [T] found coding rules

--- test/test_image.js
+++ test/test_image.js
@@ -2,4 +2,6 @@ var fs = require("fs");
 var imageData = fs.readFileSync('./test.jpg');
 console.log(Buffer.from(imageData));
 console.log(Buffer.from(imageData, "utf8"));
-console.log(Buffer.from(imageData, "ascii"));
+let encodedMessage = Buffer.from(Buffer.from(imageData, 'utf8').toString('utf8'), "binary");
+console.log(encodedMessage);
+
