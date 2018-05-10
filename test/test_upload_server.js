e53e67407660f680cf7da092cb14b642c9354b02
Phodal HUANG <h@phodal.com>
2018-05-10 20:57:15 +0800

    [T] busybox works........

--- test/test_upload_server.js
+++ test/test_upload_server.js
@@ -1,28 +1,31 @@
-var express = require("express");
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var multerupload = multer({dest: 'fileprint/'});
-var app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+var http = require('http'),
+  inspect = require('util').inspect;
 
-var router = express.Router();
-router.get('/', function (req, res) {
-  res.json({message: 'welcome to our upload module apis'});
-});
+var Busboy = require('busboy');
 
-router.post('/upload', multerupload.any(), function(req, res){
-  console.log(req);
-  res.send({
-    "code":"200",
-    "success":"files printed successfully"
-  })
+http.createServer(function(req, res) {
+  if (req.method === 'POST') {
+    var busboy = new Busboy({ headers: req.headers });
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+      console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+      file.on('data', function(data) {
+        console.log(data);
+        console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+      });
+      file.on('end', function() {
+        console.log('File [' + fieldname + '] Finished');
+      });
+    });
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    });
+    busboy.on('finish', function() {
+      console.log('Done parsing form!');
+      res.writeHead(303, { Connection: 'close', Location: '/' });
+      res.end();
+    });
+    req.pipe(busboy);
+  }
+}).listen(8000, function() {
+  console.log('Listening for requests');
 });
-
-app.use('/api', router);
-app.listen(4000);
